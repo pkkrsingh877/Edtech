a9913b3e20efef7a8dbd94f3b2c628f5d5190512
Afjal hussain <mdafjalhussain503@gmail.com>
2025-02-25 16:39:38 +0530

    gsap and home page done

--- etsec/Home/index.js
+++ etsec/Home/index.js
@@ -1,20 +1,145 @@
+const toggleBtn = document.querySelector('[data-toggle="slidder"]');
+const slidder = document.querySelector(".slidder");
 
-  const toggleBtn = document.querySelector('[data-toggle="slidder"]');
-  const slidder = document.querySelector('.slidder');
+toggleBtn.addEventListener("mouseover", () => {
+  slidder.classList.add("active");
+});
 
-  toggleBtn.addEventListener('mouseover', () => {
-    slidder.classList.add('active');
+toggleBtn.addEventListener("mouseleave", () => {
+  setTimeout(() => {
+    if (!slidder.matches(":hover")) {
+      slidder.classList.remove("active");
+    }
+  }, 100);
+});
+
+slidder.addEventListener("mouseleave", () => {
+  slidder.classList.remove("active");
+});
+
+const images = document.querySelectorAll(".img-fluid");
+// console.log(images);
+
+images.forEach((img) => {
+  img.addEventListener("mouseover", (e) => {
+    e.currentTarget.classList.add("hoverImage");
+    e.currentTarget.classList.remove("hoverImageInactive");
   });
+});
 
-  toggleBtn.addEventListener('mouseleave', () => {
-    setTimeout(() => {
-      if (!slidder.matches(':hover')) {
-        slidder.classList.remove('active');
-      }
-    }, 100);
+images.forEach((img) => {
+  img.addEventListener("mouseleave", (e) => {
+    e.currentTarget.classList.remove("hoverImage");
+    e.currentTarget.classList.add("hoverImageInactive");
   });
+});
 
-  slidder.addEventListener('mouseleave', () => {
-    slidder.classList.remove('active');
-  });
+//*****************     gsap  ******************/
 
+gsap.registerPlugin(ScrollTrigger);
+
+let time = gsap.timeline();
+
+
+time.from(".about_img",{
+  x:-60,
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.4,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".about_img",
+    start: "top 70%",
+    end: "top 0",
+    scrub: 1,
+  },
+
+})
+
+time.from(".about_content",{
+  x:70,
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.4,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".about_content",
+    start: "top 70%",
+    end: "top 0",
+    scrub: 1,
+  },
+
+})
+
+
+time.from(".categorygsap", {
+  y: 60,
+  opacity: 0,
+  delay: 0.6,
+  duration: 0.7,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".categorygsap",
+    start: "top 70%",
+    end: "top 0",
+    scrub: 1,
+  },
+});
+
+time.from(".categorygsapcs", {
+  x: 70,
+  opacity: 0,
+  delay: 0.6,
+  duration: 0.7,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".categorygsap",
+    start: "top 60%",
+    end: "top 0",
+    scrub: 1,
+  },
+});
+
+time.from(".coursegsap", {
+  y: 60,
+  opacity: 0,
+  delay: 0.6,
+  duration: 0.7,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".coursegsap",
+    start: "top 70%",
+    end: "top 0",
+    scrub: 1,
+  },
+});
+
+time.from(".coursegsap1", {
+  x: -70,
+  opacity: 0,
+  delay: 0.6,
+  duration: 0.7,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".coursegsap1",
+    start: "top 70%",
+    end: "top 0",
+    scrub: 1,
+  },
+});
+
+time.from(".coursegsap3", {
+  x: 70,
+  opacity: 0,
+  delay: 0.6,
+  duration: 0.7,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".coursegsap3",
+    start: "top 70%",
+    end: "top 0",
+    scrub: 1,
+  },
+});
+
+//card gsap
